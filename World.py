5be2fc3a0172f809a20214b7015b9d213b4251ad
notocode1 <Abaenezersos@gmail.com>
2025-04-01 18:17:28 +0300

    Update World.py

--- World.py
+++ World.py
@@ -15,7 +15,7 @@ from urllib.parse import urlparse
 # ====== CONFIG ======
 BOT_TOKEN = os.environ.get("8097964400:AAGDsqfURZ8WEpClJLzLCGPIed7p7sKjPh4")
 OWNER_ID = int(os.environ.get("OWNER_ID", "6478535414"))
-DB_URL = os.environ.get("postgresql://neondb_owner:npg_1ca4JbmBQkFs@ep-black-hill-a5q282lu-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require")
+DB_URL = os.environ.get("DB_URL")
 CHECK_INTERVAL = 60
 MAX_ENTRIES = 5
 MAX_TEXT_LENGTH = 4000

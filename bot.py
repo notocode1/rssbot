c11d1bae483d40189d4691516ec5bc9a5fec818b
notocode1 <Abaenezersos@gmail.com>
2025-04-02 16:06:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,20 +5,9 @@ from db import init_db, save_group, get_last_seen_time
 from commands import register_commands
 from feeds import start_feed_loop
 import config  # Add this line to import the config
+from utils import escape_markdown  # Make sure escape_markdown is imported
 
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode='MarkdownV2')
-
-@bot.message_handler(func=lambda msg: msg.chat.type in ['group', 'supergroup'])
-def on_group_message(msg):
-    save_group(msg.chat.id, msg.chat.title, msg.chat.type)
-
-def run_bot(config):
-    init_db()
-    start_time = get_last_seen_time() or time.time()
-    register_commands(bot)
-    start_feed_loop(bot, start_time)
-    print("🚀 Bot is running")
-    bot.infinity_polling()
 
 @bot.message_handler(func=lambda msg: msg.chat.type in ['group', 'supergroup'])
 def on_group_message(msg):
@@ -40,6 +29,14 @@ def on_group_message(msg):
         f"I'll keep you updated with the latest feeds!"
     )
     bot.send_message(msg.chat.id, welcome_message)
+
+def run_bot(config):
+    init_db()
+    start_time = get_last_seen_time() or time.time()
+    register_commands(bot)
+    start_feed_loop(bot, start_time)
+    print("🚀 Bot is running")
+    bot.infinity_polling()
 
 # Ensure that the bot runs when this file is executed directly
 if __name__ == "__main__":

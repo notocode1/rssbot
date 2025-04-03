771d626e0d70347fd0aed07a6f600a2ca80360c3
notocode1 <Abaenezersos@gmail.com>
2025-04-03 14:02:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 BOT_TOKEN = "7645110484:AAHBmNrLChJSdAJTypB6Sz7KWuesAFC_HtQ"  # Replace with your real token
-OWNER_ID = 6478535414
+OWNER_ID = '6478535414'
 DB_URL = "postgresql://Beon_owner:npg_sYXn3CLrKA4N@ep-shy-recipe-a5azsc70-pooler.us-east-2.aws.neon.tech/Beon?sslmode=require"  # Your Neon DB URL
 CHECK_INTERVAL = 60
 MAX_ENTRIES = 5

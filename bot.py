b35082c3b996826e183ea1fa87780092e98dfedc
notocode1 <Abaenezersos@gmail.com>
2025-04-03 13:47:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import time
 import telebot
 from config import BOT_TOKEN, OWNER_ID
-from db import init_db, save_group, get_last_seen_time
+from db import init_db, save_group, get_last_seen_time, is_group_saved
 from feeds import start_feed_loop
 import config
 from utils import escape_markdown
@@ -11,15 +11,16 @@ bot = telebot.TeleBot(BOT_TOKEN, parse_mode='MarkdownV2')
 @bot.message_handler(func=lambda msg: msg.chat.type in ['group', 'supergroup'])
 def on_group_message(msg):
     try:
-        # Save the group to the database
-        save_group(msg.chat.id, msg.chat.title, msg.chat.type)
-        added_message = (
-            f"🆕 New Group Added:\n\n"
-            f"*Title:* {escape_markdown(msg.chat.title, version=2)}\n"
-            f"*Chat ID:* `{msg.chat.id}`\n"
-            f"*Type:* `{msg.chat.type}`"
-        )
-        bot.send_message(OWNER_ID, added_message, parse_mode='MarkdownV2')
+        # Check if the group is already in the database
+        if not is_group_saved(msg.chat.id):
+            save_group(msg.chat.id, msg.chat.title, msg.chat.type)
+            added_message = (
+                f"🆕 New Group Added:\n\n"
+                f"*Title:* {escape_markdown(msg.chat.title, version=2)}\n"
+                f"*Chat ID:* `{msg.chat.id}`\n"
+                f"*Type:* `{msg.chat.type}`"
+            )
+            bot.send_message(OWNER_ID, added_message, parse_mode='MarkdownV2')
     except Exception as e:
         print(f"Error processing group message: {e}")  # Print error if something goes wrong
 

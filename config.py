81b92dcd172f881772d02b808831c2ff2b924f3d
notocode1 <Abaenezersos@gmail.com>
2025-04-04 18:26:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7645110484:AAFzTPA1T6SoNjUgevx8T_1IIP2NAq3810A")
 OWNER_ID = int(os.getenv("OWNER_ID", "6478535414"))
 DB_URL = os.getenv("DATABASE_URL", "postgresql://Beon_owner:npg_sYXn3CLrKA4N@ep-shy-recipe-a5azsc70-pooler.us-east-2.aws.neon.tech/Beon?sslmode=require")
-CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL", 10800))  # 3 hours
+CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL", 108))  # 3 hours
 MAX_ENTRIES = 5
 MAX_TEXT_LENGTH = 1000  # match your strict rule
 
